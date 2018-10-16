dff05ed42fd7a605ab36f2aac28cc339265b3f99
Calin Farcas <cf316@ic.ac.uk>
2018-10-16 13:02:28 +0100

    changed emit event syntax such that only emit functionCall is accepted instead of emit expression

--- Sources/AST/ASTDumper.swift
+++ Sources/AST/ASTDumper.swift
@@ -503,7 +503,7 @@ public class ASTDumper {
   func dump(_ emitStatement: EmitStatement) {
     writeNode("EmitStatement") {
       self.dump(emitStatement.emitToken)
-      self.dump(emitStatement.expression)
+      self.dump(emitStatement.functionCall)
     }
   }
 
