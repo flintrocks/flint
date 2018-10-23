7a9f656786aeb1bb0cb7cd61f0cf1e3b53d885b0
Nik Vangerow <n@vangerow.uk>
2018-10-23 12:40:32 +0100

    disable IRPreprocessor step for Trait

--- Sources/IRGen/Preprocessor/IRPreprocessor.swift
+++ Sources/IRGen/Preprocessor/IRPreprocessor.swift
@@ -29,6 +29,14 @@ public struct IRPreprocessor: ASTPass {
     return ASTPassResult(element: structMember, diagnostics: [], passContext: passContext)
   }
 
+  public func process(traitDeclaration: TraitDeclaration, passContext: ASTPassContext) -> ASTPassResult<TraitDeclaration> {
+    var traitDeclaration = traitDeclaration
+    // Replace trait members with empty list as we NEEDN'T process the trait (IR does not get generated).
+    // This was necessary as this subtree does unnecessary type checking and breaks with the addition of Self.
+    traitDeclaration.members = []
+    return ASTPassResult(element: traitDeclaration, diagnostics: [], passContext: passContext)
+  }
+
   /// Returns assignment statements for all the properties which have been assigned default values.
   func defaultValueAssignments(in passContext: ASTPassContext) -> [Statement] {
     let enclosingType = passContext.enclosingTypeIdentifier!.name
