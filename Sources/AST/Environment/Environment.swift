930e62f405ce987923198337f5e9682c5ac75738
Nik Vangerow <n@vangerow.uk>
2018-10-26 18:13:27 +0100

    update description of function argument matcher

--- Sources/AST/Environment/Environment.swift
+++ Sources/AST/Environment/Environment.swift
@@ -152,11 +152,12 @@ public struct Environment {
     return true
   }
 
-  /// Whether to function arguments are compatible.
+  /// Whether two function arguments are compatible.
   ///
   /// - Parameters:
-  ///   - source: arguments of the function that the user is trying to call.
+  ///   - source: arguments of the function that the user is trying to use.
   ///   - target: arguments of the function available in this scope.
+  ///   - enclosingType: Type identifier of type containing *source* function.
   /// - Returns: Boolean indicating whether function arguments are compatible.
   func areFunctionArgumentsCompatible(source: [RawType], target: [RawType], enclosingType: RawTypeIdentifier) -> Bool {
     // If source contains an argument of self type then attempt to replace with enclosing type
