2aaacc21ed1f5762c73d2e6dace3707c8e4d61ed
Nik Vangerow <n@vangerow.uk>
2018-11-04 08:52:35 +0000

    add IRPreprocessorTests

---
+++ Tests/IRGenTests/IRPreprocessorTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import IRGen
+import AST
+
+// MARK: Tests
+final class IRPreprocessorTests: XCTestCase {
+
+  func testIRPreprocessor() {
+//    var mangler = StubMangler()
+//    mangler.stub_isMem = { (_ name: String) -> String in
+//      return ""
+//      return ""
+//    }
+//
+//    let preprocessor = IRPreprocessor(mangler: mangler)
+  }
+
+  static var allTests: [(String, () -> Void)] = []
+}
