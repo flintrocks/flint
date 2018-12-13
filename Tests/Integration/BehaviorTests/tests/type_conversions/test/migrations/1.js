50b2254d04701005b3cebd5162d891a7686f523a
Nik Vangerow <n@vangerow.uk>
2018-12-13 16:41:49 +0000

    test casting protection

--- Tests/Integration/BehaviorTests/tests/type_conversions/test/migrations/1.js
+++ Tests/Integration/BehaviorTests/tests/type_conversions/test/migrations/1.js
@@ -1,7 +1,9 @@
 var config = require("../config.js")
 
 var Contract = artifacts.require("./" + config.contractName + ".sol");
+var External = artifacts.require("./External.sol");
 
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(Contract, accounts[9]);
+module.exports = function(deployer) {
+  deployer.deploy(Contract);
+  deployer.deploy(External);
 };
--- Tests/Integration/BehaviorTests/tests/type_conversions/test/test/test.js
+++ Tests/Integration/BehaviorTests/tests/type_conversions/test/test/test.js
@@ -6,85 +6,43 @@ var Contract = artifacts.require("./" + config.contractName + ".sol");
 var Interface = artifacts.require("./_Interface" + config.contractName + ".sol");
 Contract.abi = Interface.abi
 
-contract(config.contractName, function(accounts) {
- it("should be possible for the owner to add another owner", async function() {
+var ExternalContract = artifacts.require("./External.sol");
+
+contract(config.contractName, function() {
+  it("should be possible to cast from a lower bit type to a higher bit type", async function() {
+    const external = await ExternalContract.deployed();
     const instance = await Contract.deployed();
+
     let t;
+    t = await instance.testReduce(100, external.address);
+    assert.equal(t.valueOf(), 100);
+  });
 
-    await instance.addOwner(accounts[0], {from: accounts[9]});
-    t = await instance.getOwner(0, {from: accounts[0]});
-    assert.equal(t.valueOf(), accounts[9]);
-    t = await instance.getOwner(1, {from: accounts[9]});
-    assert.equal(t.valueOf(), accounts[0]);
- });
-
- it("should be possible for the original owner to access all ifYouCan functions", async function() {
-   const instance = await Contract.deployed();
-   let t;
-
-   t = await instance.ifYouCan({from: accounts[9]});
-   assert.equal(t.valueOf(), 1);
-
-   t = await instance.ifYouCan2({from: accounts[9]});
-   assert.equal(t.valueOf(), 1);
-
-   t = await instance.ifYouCan3({from: accounts[9]});
-   assert.equal(t.valueOf(), 1);
-
-   t = await instance.ifYouCan4({from: accounts[9]});
-   assert.equal(t.valueOf(), 42);
-
-   t = await instance.ifYouCan5({from: accounts[9]});
-   assert.equal(t.valueOf(), 42);
- });
-
- it("should be possible for the original owner to check they can access functions", async function() {
-   const instance = await Contract.deployed();
-   let t;
-
-   t = await instance.check({from: accounts[9]});
-   assert.equal(t.valueOf(), 1);
-
-   t = await instance.check2({from: accounts[9]});
-   assert.equal(t.valueOf(), 1);
- });
-
- it("should be possible for anyone to check that they can't access functions", async function() {
-   const instance = await Contract.deployed();
-   let t;
-
-   t = await instance.check({from: accounts[1]});
-   assert.equal(t.valueOf(), 0);
-
-   t = await instance.check2({from: accounts[1]});
-   assert.equal(t.valueOf(), 0);
- });
-
- it("should be possible for a member of owners to access all functions", async function() {
+  it("should be possible to cast from a higher bit type to a lower bit type for small values", async function() {
+    const external = await ExternalContract.deployed();
     const instance = await Contract.deployed();
+
     let t;
+    const value = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF / 2;
+    t = await instance.testIncrease(value, external.address);
+    assert.equal(t.valueOf(), value);
+  });
 
-    await instance.addOwner(accounts[0], {from: accounts[9]});
+  it("should not be possible to cast from a lower bit type to a higher bit type", async function() {
+    const external = await ExternalContract.deployed();
+    const instance = await Contract.deployed();
+    const value = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
-    t = await instance.ifYouCan({from: accounts[0]});
-    assert.equal(t.valueOf(), 1);
+    try {
+      await instance.testIncrease(value, external.address);
+      assert(false);
+    } catch (e) {
+      if (e.message == "VM Exception while processing transaction: revert") {
+        assert(true);
+      } else {
+        throw e;
+      }
+    }
+  });
 
-    t = await instance.ifYouCan2({from: accounts[0]});
-    assert.equal(t.valueOf(), 1);
-
-    t = await instance.ifYouCan3({from: accounts[0]});
-    assert.equal(t.valueOf(), 1);
-
-    t = await instance.ifYouCan4({from: accounts[0]});
-    assert.equal(t.valueOf(), 42);
-
-    t = await instance.ifYouCan5({from: accounts[0]});
-    assert.equal(t.valueOf(), 42);
-
-    t = await instance.check({from: accounts[0]});
-    assert.equal(t.valueOf(), 1);
-
-    t = await instance.check2({from: accounts[0]});
-    assert.equal(t.valueOf(), 1);
-    });
 });
